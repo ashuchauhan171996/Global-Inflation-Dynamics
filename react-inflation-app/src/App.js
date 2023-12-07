110a0dc87317d6170865e692aa2706275cc0c6b4
ashutoshchauhan <ashuchauhan171996@gmail.com>
2023-12-07 00:00:27 -0600

    added observations

--- react-inflation-app/src/App.js
+++ react-inflation-app/src/App.js
@@ -72,6 +72,13 @@ function App() {
       <h5>A. Global Inflation variation with time (1970 - 2023)</h5>
       </div>
       <Cartogram/>
+      <h5>General Obervations:</h5>
+      <p>* <b className='bold-color'>1973-1974 Oil Crisis:</b> The OPEC oil embargo led to a spike in oil prices, causing a surge in inflation globally. We see most of the countries in yellow, especially the Americas and Africa. This means high inflation throughout the world except parts of Asia like Russia which are rich in Oil themselves. </p>
+      <p>* <b className='bold-color'>1980-1981 Volcker Shock</b> The U.S. Federal Reserve, led by Paul Volcker, raised interest rates sharply to combat inflation, contributing to a global recession. We can see a peak for all countries in the line graph below, suggesting surge in global inflation in 1980. The cartogram is also mostly yellow green for this year.</p>
+      <p>* <b className='bold-color'>1991 Dissolution of the Soviet Union:</b> The Soviet Union collapses, leading to the emergence of new independent states and significant geopolitical changes. The line graph clearly indicates a bad time for Russia in the 1990s following this crisis. </p>
+      <p>* <b className='bold-color'>1997-1998 Asian Financial Crisis:</b> Several Asian economies experienced severe economic downturns, affecting global inflation. We see from the cartogram below, that most of the Asian continent is yellow, suggesting high inflation in 1997. Similar results are observed for 1998 as well, indicating high inflation, as confirmed by the real facts.</p>
+      <p>* <b className='bold-color'>2007-2008 Global Financial Crisis:</b>The collapse of major financial institutions led to a worldwide economic downturn, prompting central banks to implement unconventional monetary policies.</p>
+      <p>* <b className='bold-color'>2020 COVID-19 Pandemic:</b>The global pandemic disrupted economies worldwide, leading to both supply and demand shocks, with varying effects on inflation. In the line graph below, we see a rise in inflation from 2020-2022, and the cause of this as we all know is the Covid Pandemic that struck us.</p>
 
       <hr />
       <div className='body-component-heading'>
